39d6fc982c6707ef8c84a80c67783d7f3ee2bce0
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-25 23:01:27 +0000

    Membership Class
    Adding

---
+++ src/Customers/MembershipCard.java
@@ -0,0 +1,16 @@
+package Customers;
+
+public class MembershipCard {
+
+	private int numbMember;
+	private String typeMember;
+	private String cardHolderName;
+	private int cardNumb;
+	private int cardExpired;
+	private int cardCVV;
+	private int loyaltyPoints;
+	
+	
+	
+	
+}
