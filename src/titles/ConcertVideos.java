43ac0ab85a53825e009fb99b7105cb6be505091e
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-25 21:10:18 +0000

    ConcertVideo Class
    Adding

---
+++ src/titles/ConcertVideos.java
@@ -0,0 +1,11 @@
+package titles;
+
+public class ConcertVideos extends Music{
+
+	public ConcertVideos(String title, String album, String band, String yearRelease, String genre) {
+		super(title, album, band, yearRelease, genre);
+		
+	}
+
+	
+}
