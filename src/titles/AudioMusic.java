4fd2def3976a0c1c97a34f691ef135011046484e
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-25 21:05:10 +0000

    AudioMusic Class
    Adding

---
+++ src/titles/AudioMusic.java
@@ -0,0 +1,12 @@
+package titles;
+
+public class AudioMusic extends Music {
+
+	public AudioMusic(String title, String album, String band, String yearRelease, String genre) {
+		super(title, album, band, yearRelease, genre);
+		
+	}
+
+	
+
+}
