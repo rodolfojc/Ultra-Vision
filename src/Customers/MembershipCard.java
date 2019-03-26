957e34e66474b38c61a42d2bc2728069da747472
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-26 20:18:35 +0000

    Customer Clases
    Added super class customers and subclases musiclovers, videolovers, tvlover, premiunlovers. Also class for debit or credit card instances

--- src/Customers/MembershipCard.java
+++ src/Customers/MembershipCard.java
@@ -1,16 +1,41 @@
-package Customers;
+package customers;
 
 public class MembershipCard {
 
-	private int numbMember;
-	private String typeMember;
-	private String cardHolderName;
-	private int cardNumb;
-	private int cardExpired;
-	private int cardCVV;
-	private int loyaltyPoints;
+	private int points;
+	private boolean freeRentAllowed;
+	   
+	   public void addPoints(int points){
+			this.points += points;
+			setRentAllowed();
+	   }
+	   
+	   public boolean availFreeRent(){
+			if(this.isfreeRentAllowed()){
+				this.points -= 100;
+				setRentAllowed();
+				return true;
+			} else {
+				return false;
+			}
+	   }
+	   
+	   private void setRentAllowed(){
+			if (this.points >= 100){
+				this.freeRentAllowed = true;
+			} else {
+				this.freeRentAllowed = false;
+			}
+	   }
+	   
+	   public int getPoints() {
+			return  points;
+    }
+	   
+	   public boolean isfreeRentAllowed(){
+			return freeRentAllowed;
+	   
+	   }
 	
-	
-	
-	
+		
 }
---
+++ src/customers/Customer.java
@@ -0,0 +1,12 @@
+package customers;
+
+public abstract class Customer {
+
+	private String custName;
+	private String custSurname;
+	private String birthday;
+	private MembershipCard myMemberCard;
+	private DebitCreditCard myCard;
+	
+	
+}
