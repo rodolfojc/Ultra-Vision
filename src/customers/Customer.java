c158329b93e634d26e1d818cab5f2ccd88da7cf0
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-27 22:07:35 +0000

    Customer Super Class
    Adding constructor, getters and setters.

--- src/customers/Customer.java
+++ src/customers/Customer.java
@@ -11,5 +11,56 @@ public abstract class Customer {
 	private MembershipCard myMemberCard;
 	private DebitCreditCard myCard;
 	
+	public Customer(String custName, String custSurname, String birthday, MembershipCard myMemberCard,
+			DebitCreditCard myCard) {
+		this.custName = custName;
+		this.custSurname = custSurname;
+		this.birthday = birthday;
+		this.myMemberCard = myMemberCard;
+		this.myCard = myCard;
+	}
+
+	public String getCustName() {
+		return custName;
+	}
+
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+
+	public String getCustSurname() {
+		return custSurname;
+	}
+
+	public void setCustSurname(String custSurname) {
+		this.custSurname = custSurname;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public MembershipCard getMyMemberCard() {
+		return myMemberCard;
+	}
+
+	public void setMyMemberCard(MembershipCard myMemberCard) {
+		this.myMemberCard = myMemberCard;
+	}
+
+	public DebitCreditCard getMyCard() {
+		return myCard;
+	}
+
+	public void setMyCard(DebitCreditCard myCard) {
+		this.myCard = myCard;
+	}
+	
+	
+	
 	
 }
