420c6dd61745b9e3b89c523bc4a4ed2ab4f3e87b
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-28 21:00:25 +0000

    Abstract Tv Class Added
    General  structure changed.

--- src/titles/ConcertVideos.java
+++ src/titles/ConcertVideos.java
@@ -3,7 +3,7 @@ package titles;
 import titles.interfaces.BlueRay;
 import titles.interfaces.DVD;
 
-public class ConcertVideos extends Music implements DVD, BlueRay{
+public class ConcertVideos extends Music implements DVD, BlueRay {
 
 	public ConcertVideos(String title, String yearRelease, String genre, String album, String band) {
 		super(title, yearRelease, genre, album, band);
--- src/titles/Movie.java
+++ src/titles/Movie.java
@@ -3,55 +3,12 @@ package titles;
 import titles.interfaces.BlueRay;
 import titles.interfaces.DVD;
 
-public class Movie extends Title implements DVD, BlueRay {
+public class Movie extends Tv implements DVD, BlueRay {
 
-	private String director;
-	private String runningTime;
-	private String languages;
-	private String country;
-	
-	public Movie(String title, String yearRelease, String genre, String director, String runningTime, 
-			String languages, String country) {
-		super(title, yearRelease, genre);
-		this.director = director;
-		this.runningTime = runningTime;
-		this.languages = languages;
-		this.country = country;
+	public Movie(String title, String yearRelease, String genre, String director, String runningTime, String languages,
+			String country) {
+		super(title, yearRelease, genre, director, runningTime, languages, country);
 		
 	}
 
-	public String getDirector() {
-		return director;
-	}
-
-	public void setDirector(String director) {
-		this.director = director;
-	}
-
-	public String getRunningTime() {
-		return runningTime;
-	}
-
-	public void setRunningTime(String runningTime) {
-		this.runningTime = runningTime;
-	}
-
-	public String getLanguages() {
-		return languages;
-	}
-
-	public void setLanguages(String languages) {
-		this.languages = languages;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	
-	
-	
 }
