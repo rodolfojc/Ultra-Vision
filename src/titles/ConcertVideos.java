aa25d63fe99f4fdcd97beca4bb80c12819beb4a5
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-31 22:19:05 +0100

    ConcertVideos Imlementation
    As video concert also can be found in dvd and blueray format, now added varibles and implementations.

--- src/titles/ConcertVideos.java
+++ src/titles/ConcertVideos.java
@@ -8,21 +8,20 @@ public class ConcertVideos extends Music implements DVD, BlueRay {
 	private boolean dvd;
 	private boolean blueray;
 	
-	public ConcertVideos(String title, String yearRelease, String genre, String album, String band) {
+	public ConcertVideos(String title, String yearRelease, String genre, String album, String band, boolean dvd, boolean blueray) {
 		super(title, yearRelease, genre, album, band);
-		
+		this.dvd = dvd;
+		this.blueray = blueray;
 	}
 
 	@Override
 	public boolean isBlueRayFormat() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.blueray;
 	}
 
 	@Override
 	public boolean isDVDformat() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.dvd;
 	}
 
 	
