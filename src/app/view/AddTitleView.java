3b6f8abb21d45e23bec7a11c40e44a3421dcffc8
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-04-10 22:38:55 +0100

    Add Title View
    Added AddTitleView class with class controller

---
+++ src/app/view/AddTitleView.java
@@ -0,0 +1,14 @@
+package app.view;
+
+import app.controller.AddTitleController;
+
+public class AddTitleView extends GuiView {
+
+	AddTitleController myController;
+	
+	public AddTitleView(String name, int width, int height, boolean Resizable, AddTitleController myController) {
+		super(name, width, height, Resizable);
+		this.myController = myController;
+	}
+
+}
