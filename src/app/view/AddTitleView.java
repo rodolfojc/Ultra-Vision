8a213ba9e83a9976f1f7d4780d908097f202cfe9
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-04-11 23:52:48 +0100

    Add title - Format options
    Format options added

--- src/app/view/AddTitleView.java
+++ src/app/view/AddTitleView.java
@@ -21,9 +21,10 @@ public class AddTitleView extends GuiView {
 		String[] myOpts = {"MusicCD", "Video Concert"};
 		String[] myOptsTwo = {"TV/Boxset", "Movie"};
 		String[] format = {"CD"};
+		String[] yesOrNo = {"Yes","No"};
 		
 		JPanel type = new JPanel();
-		this.setGrid(14, 0, type);
+		this.setGrid(16, 0, type);
 		
 		this.addLabel("Select Type", type);
 		JComboBox myBox = this.addComboB(myOpts, type);
@@ -42,11 +43,10 @@ public class AddTitleView extends GuiView {
 		JComboBox myBoxTwo = this.addComboB(format, type);
 		
 		JPanel typeTwo = new JPanel();
-		this.setGrid(14, 0, typeTwo);
+		this.setGrid(16, 0, typeTwo);
 		
 		this.addLabel("Select Type", typeTwo);
 		JComboBox myBoxThree = this.addComboB(myOptsTwo, typeTwo);
-		
 		this.addLabel("Title name", typeTwo);
 		this.addTextField(20, typeTwo);
 		this.addLabel("Year release", typeTwo);
@@ -59,7 +59,14 @@ public class AddTitleView extends GuiView {
 		this.addTextField(20, typeTwo);
 		this.addLabel("Country", typeTwo);
 		this.addTextField(20, typeTwo);
-				
+		this.addLabel("Format", typeTwo);
+		JPanel formatPanel = new JPanel();
+		this.addLabel("DVD", formatPanel);
+		JComboBox myBoxFour = this.addComboB(yesOrNo, formatPanel);
+		this.addLabel("Blue-Ray", formatPanel);
+		JComboBox myBoxFive = this.addComboB(yesOrNo, formatPanel);
+		typeTwo.add(formatPanel);
+		
 		this.panel.add(type);
 		this.panel.add(typeTwo);
 		
