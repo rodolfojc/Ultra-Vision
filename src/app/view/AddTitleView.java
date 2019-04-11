b1f4dd07526846a0576d044971bd7ecb71f7f581
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-04-11 23:46:13 +0100

    Add title - Movie / BoxSet
    Building up the interface

--- src/app/view/AddTitleView.java
+++ src/app/view/AddTitleView.java
@@ -19,9 +19,11 @@ public class AddTitleView extends GuiView {
 		
 		this.setGrid(0, 2, this.panel);
 		String[] myOpts = {"MusicCD", "Video Concert"};
+		String[] myOptsTwo = {"TV/Boxset", "Movie"};
+		String[] format = {"CD"};
 		
 		JPanel type = new JPanel();
-		this.setGrid(12, 0, type);
+		this.setGrid(14, 0, type);
 		
 		this.addLabel("Select Type", type);
 		JComboBox myBox = this.addComboB(myOpts, type);
@@ -36,25 +38,28 @@ public class AddTitleView extends GuiView {
 		this.addTextField(20, type);
 		this.addLabel("Genre", type);
 		this.addTextField(20, type);
+		this.addLabel("Format", type);
+		JComboBox myBoxTwo = this.addComboB(format, type);
 		
 		JPanel typeTwo = new JPanel();
-		this.setGrid(12, 0, typeTwo);
+		this.setGrid(14, 0, typeTwo);
 		
 		this.addLabel("Select Type", typeTwo);
-		JComboBox myBoxTwo = this.addComboB(myOpts, typeTwo);
+		JComboBox myBoxThree = this.addComboB(myOptsTwo, typeTwo);
 		
 		this.addLabel("Title name", typeTwo);
-		this.addTextField(20, typeTwo);
-		this.addLabel("Band", typeTwo);
-		this.addTextField(20, typeTwo);
-		this.addLabel("Album", typeTwo);
 		this.addTextField(20, typeTwo);
 		this.addLabel("Year release", typeTwo);
 		this.addTextField(20, typeTwo);
 		this.addLabel("Genre", typeTwo);
 		this.addTextField(20, typeTwo);
-		
-		
+		this.addLabel("Director", typeTwo);
+		this.addTextField(20, typeTwo);
+		this.addLabel("Running time", typeTwo);
+		this.addTextField(20, typeTwo);
+		this.addLabel("Country", typeTwo);
+		this.addTextField(20, typeTwo);
+				
 		this.panel.add(type);
 		this.panel.add(typeTwo);
 		
