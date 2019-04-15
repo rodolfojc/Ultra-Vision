51b01d7a3cf3921b1c831abf3c289c373a10b856
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-04-15 20:15:22 +0100

    Add Title - Tv Attributes
    Added Tv attributes. Also getter and setter

--- src/app/view/AddTitleView.java
+++ src/app/view/AddTitleView.java
@@ -15,16 +15,29 @@ import app.controller.AddTitleController;
 
 public class AddTitleView extends GuiView {
 
-	AddTitleController myController;
 	//MUSIC ATTRIBUTES
-	private JTextField titleName;
-	private JTextField band;
-	private JTextField album;
-	private JTextField yearRelease;
-	private JTextField genre;
+	private JTextField musicTitleName;
+	private JTextField musicBand;
+	private JTextField musicAlbum;
+	private JTextField musicYearRelease;
+	private JTextField musicGenre;
 	private JComboBox isDVDMusic;
 	private JComboBox isBlueMusic;
 	private JComboBox musicType;
+	private JComboBox tvType;
+	
+	//TV ATTRIBUTES
+	private JTextField tvTitleName;
+	private JTextField tvYearRelease;
+	private JTextField tvGenre;
+	private JTextField tvDirector;
+	private JTextField tvRunningTime;
+	private JTextField country;
+	private JTextField language;
+	private JComboBox isDVDTv;
+	private JComboBox isBlueTv;
+	
+	AddTitleController myController;
 	
 	
 	public AddTitleView(String name, int width, int height, boolean Resizable, AddTitleController myController) {
@@ -61,13 +74,231 @@ public class AddTitleView extends GuiView {
 		panel.add(myComboBox);
 		return myComboBox;
 	}
+	
+	
+
+	public JTextField getMusicTitleName() {
+		return musicTitleName;
+	}
+
+
+
+	public void setMusicTitleName(JTextField musicTitleName) {
+		this.musicTitleName = musicTitleName;
+	}
+
+
+
+	public JTextField getMusicBand() {
+		return musicBand;
+	}
+
+
+
+	public void setMusicBand(JTextField musicBand) {
+		this.musicBand = musicBand;
+	}
+
+
+
+	public JTextField getMusicAlbum() {
+		return musicAlbum;
+	}
+
+
+
+	public void setMusicAlbum(JTextField musicAlbum) {
+		this.musicAlbum = musicAlbum;
+	}
+
+
+
+	public JTextField getMusicYearRelease() {
+		return musicYearRelease;
+	}
+
+
+
+	public void setMusicYearRelease(JTextField musicYearRelease) {
+		this.musicYearRelease = musicYearRelease;
+	}
+
+
+
+	public JTextField getMusicGenre() {
+		return musicGenre;
+	}
+
+
+
+	public void setMusicGenre(JTextField musicGenre) {
+		this.musicGenre = musicGenre;
+	}
+
+
+
+	public JComboBox getIsDVDMusic() {
+		return isDVDMusic;
+	}
+
+
+
+	public void setIsDVDMusic(JComboBox isDVDMusic) {
+		this.isDVDMusic = isDVDMusic;
+	}
+
+
+
+	public JComboBox getIsBlueMusic() {
+		return isBlueMusic;
+	}
+
+
+
+	public void setIsBlueMusic(JComboBox isBlueMusic) {
+		this.isBlueMusic = isBlueMusic;
+	}
+
+
+
+	public JComboBox getMusicType() {
+		return musicType;
+	}
+
+
+
+	public void setMusicType(JComboBox musicType) {
+		this.musicType = musicType;
+	}
+
+
+
+	public JComboBox getTvType() {
+		return tvType;
+	}
+
+
+
+	public void setTvType(JComboBox tvType) {
+		this.tvType = tvType;
+	}
+
+
+
+	public JTextField getTvTitleName() {
+		return tvTitleName;
+	}
+
+
+
+	public void setTvTitleName(JTextField tvTitleName) {
+		this.tvTitleName = tvTitleName;
+	}
+
+
+
+	public JTextField getTvYearRelease() {
+		return tvYearRelease;
+	}
+
+
+
+	public void setTvYearRelease(JTextField tvYearRelease) {
+		this.tvYearRelease = tvYearRelease;
+	}
+
+
+
+	public JTextField getTvGenre() {
+		return tvGenre;
+	}
+
+
+
+	public void setTvGenre(JTextField tvGenre) {
+		this.tvGenre = tvGenre;
+	}
+
+
+
+	public JTextField getTvDirector() {
+		return tvDirector;
+	}
+
+
+
+	public void setTvDirector(JTextField tvDirector) {
+		this.tvDirector = tvDirector;
+	}
+
+
+
+	public JTextField getTvRunningTime() {
+		return tvRunningTime;
+	}
+
+
+
+	public void setTvRunningTime(JTextField tvRunningTime) {
+		this.tvRunningTime = tvRunningTime;
+	}
+
+
+
+	public JTextField getCountry() {
+		return country;
+	}
+
+
+
+	public void setCountry(JTextField country) {
+		this.country = country;
+	}
+
+
+
+	public JTextField getLanguage() {
+		return language;
+	}
+
+
+
+	public void setLanguage(JTextField language) {
+		this.language = language;
+	}
+
+
+
+	public JComboBox getIsDVDTv() {
+		return isDVDTv;
+	}
+
+
+
+	public void setIsDVDTv(JComboBox isDVDTv) {
+		this.isDVDTv = isDVDTv;
+	}
+
+
+
+	public JComboBox getIsBlueTv() {
+		return isBlueTv;
+	}
+
+
+
+	public void setIsBlueTv(JComboBox isBlueTv) {
+		this.isBlueTv = isBlueTv;
+	}
+
+
 
 	private void setFrame() {
 		
 		this.setGrid(0, 2, this.panel);
 		String[] myOpts = {"MusicCD", "Video Concert"};
 		String[] myOptsTwo = {"TV/Boxset", "Movie"};
-		String[] format = {"CD"};
+		//String[] format = {"CD"};
 		String[] yesOrNo = {"Yes","No","N/A"};
 		
 		JPanel type = new JPanel();
@@ -77,15 +308,15 @@ public class AddTitleView extends GuiView {
 		musicTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		this.musicType = this.addComboB(myOpts, type);
 		this.addLabel("Title name", type);
-		this.titleName = this.addTextField(20, type);
+		this.musicTitleName = this.addTextField(20, type);
 		this.addLabel("Band", type);
-		this.band = this.addTextField(20, type);
+		this.musicBand = this.addTextField(20, type);
 		this.addLabel("Album", type);
-		this.album = this.addTextField(20, type);
+		this.musicAlbum = this.addTextField(20, type);
 		this.addLabel("Year release", type);
-		this.yearRelease = this.addTextField(20, type);
+		this.musicYearRelease = this.addTextField(20, type);
 		this.addLabel("Genre", type);
-		this.genre = this.addTextField(20, type);
+		this.musicGenre = this.addTextField(20, type);
 		this.addLabel("", type);
 		this.addLabel("", type);
 		this.addLabel("Format", type);
@@ -100,39 +331,41 @@ public class AddTitleView extends GuiView {
 		type.add(formatPanelOne);
 		this.addLabel("", type);
 		this.addLabel("", type);
-		JButton addTitle = this.addButton("Add", type);
+		JButton addMusicTitle = this.addButton("Add Music", type);
 		this.addLabel("", type);
 		type.setBorder(new EmptyBorder(new Insets(0, 0, 0, 25)));
 		
 		
 		JPanel typeTwo = new JPanel();
-		this.setGrid(20, 0, typeTwo);
-		JLabel tvType = this.addLabel("TV (Type)", typeTwo);
-		tvType.setFont(new Font("Serif", Font.BOLD, 24));
-		tvType.setHorizontalAlignment(SwingConstants.CENTER);
-		JComboBox myBoxThree = this.addComboB(myOptsTwo, typeTwo);
+		this.setGrid(22, 0, typeTwo);
+		JLabel tvTypeTitle = this.addLabel("TV (Type)", typeTwo);
+		tvTypeTitle.setFont(new Font("Serif", Font.BOLD, 24));
+		tvTypeTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		this.tvType = this.addComboB(myOptsTwo, typeTwo);
 		this.addLabel("Title name", typeTwo);
-		this.addTextField(20, typeTwo);
+		this.tvTitleName = this.addTextField(20, typeTwo);
 		this.addLabel("Year release", typeTwo);
-		this.addTextField(20, typeTwo);
+		this.tvYearRelease = this.addTextField(20, typeTwo);
 		this.addLabel("Genre", typeTwo);
-		this.addTextField(20, typeTwo);
+		this.tvGenre = this.addTextField(20, typeTwo);
 		this.addLabel("Director", typeTwo);
-		this.addTextField(20, typeTwo);
+		this.tvDirector = this.addTextField(20, typeTwo);
 		this.addLabel("Running time", typeTwo);
-		this.addTextField(20, typeTwo);
+		this.tvRunningTime = this.addTextField(20, typeTwo);
 		this.addLabel("Country", typeTwo);
-		this.addTextField(20, typeTwo);
+		this.country = this.addTextField(20, typeTwo);
+		this.addLabel("Language", typeTwo);
+		this.language = this.addTextField(20, typeTwo);
 		this.addLabel("Format", typeTwo);
 		JPanel formatPanelTwo = new JPanel();
 		this.addLabel("DVD ", formatPanelTwo);
-		JComboBox myBoxFour = this.addComboB(yesOrNo, formatPanelTwo);
+		this.isDVDTv = this.addComboB(yesOrNo, formatPanelTwo);
 		this.addLabel("Blue-Ray ", formatPanelTwo);
-		JComboBox myBoxFive = this.addComboB(yesOrNo, formatPanelTwo);
+		this.isBlueTv = this.addComboB(yesOrNo, formatPanelTwo);
 		typeTwo.add(formatPanelTwo);
 		this.addLabel("", typeTwo);
 		this.addLabel("", typeTwo);
-		JButton addTitleTwo = this.addButton("Add", typeTwo);
+		JButton addTvTitle = this.addButton("Add Tv", typeTwo);
 		this.addLabel("", typeTwo);
 		typeTwo.setBorder(new EmptyBorder(new Insets(0, 25, 0, 0)));
 		
