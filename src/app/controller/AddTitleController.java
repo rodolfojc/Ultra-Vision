9f30cbcd50b0a9285a0b6edaa345ed09f47f08b0
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-04-15 20:54:56 +0100

    Add Title - Audio Music
    Building Up

--- src/app/controller/AddTitleController.java
+++ src/app/controller/AddTitleController.java
@@ -3,29 +3,53 @@ package app.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import app.database.Database;
+import app.model.AddTitleModel;
 import app.view.AddTitleView;
+import titles.AudioMusic;
 import titles.Title;
 
 public class AddTitleController implements ActionListener{
 
-	AddTitleView addTitleView;
-	Title title;
+	private AddTitleView addTitleView;
+	private AddTitleModel addTitleModel;
+	private Database database;
+	private Title title;
+	
 	
 	public AddTitleController(AddTitleView addTitleView) {
 		this.addTitleView = addTitleView;
 	}
 	
+	public Title getTitle() {
+		return title;
+	}
+
+	public void setTitle(Title title) {
+		this.title = title;
+	}
+		
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getActionCommand().equals("Add Music")) {
 			
-			if(addTitleView.) {
+			if(addTitleView.getMusicType().getSelectedIndex() == 0) {
+				this.setTitle(new AudioMusic(this.addTitleView.getMusicTitleName().getText(),
+											this.addTitleView.getMusicYearRelease().getText(),
+											this.addTitleView.getMusicGenre().getText(),
+											this.addTitleView.getMusicAlbum().getText(),
+											this.addTitleView.getMusicBand().getText()));
 				
+				this.database = new Database();
+				this.addTitleModel = new AddTitleModel(this.database, this.title);
+			
 			}
 			
 		}
 		
 	}
+
+	
 
 }
---
+++ src/app/model/AddTitleModel.java
@@ -0,0 +1,27 @@
+package app.model;
+
+import app.database.Database;
+import titles.Title;
+
+public class AddTitleModel {
+
+	private Title myTitle;
+	private Database myDB;
+	
+	
+	public AddTitleModel(Database database, Title title) {
+		
+		this.myTitle = title;
+		this.myDB = database;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
